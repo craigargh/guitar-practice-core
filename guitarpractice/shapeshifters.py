63050d9009f73f2e1b38bdfc900d22417d037117
Craig Richardson <craigargh@gmail.com>
2020-06-14 20:34:24 +0100

    Add placehold for shape shifters

---
+++ guitarpractice/shapeshifters.py
@@ -0,0 +1,8 @@
+from guitarpractice.models import GuitarShape
+
+
+def repeat_each_position(shape: GuitarShape, repeats: int = 2) -> GuitarShape:
+    """
+    Play each fret in the shape two or mote times
+    """
+    pass
