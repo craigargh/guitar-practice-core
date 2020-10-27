abb0b4744b94ca9c1bc9aec191991988b9660799
Craig Richardson <craigargh@gmail.com>
2020-10-27 12:44:44 +0000

    Spike: try out tab bars

--- guitarpractice/formatters.py
+++ guitarpractice/formatters.py
@@ -5,12 +5,17 @@ def to_vextab(exercise: Sequence) -> str:
     """
     http://vexflow.com/vextab/tutorial.html
     """
-    elements = []
+    elements = ['|:']
     for note in exercise.notes:
         note_el = f":{int(note.duration.division / note.duration.duration)} {note.position.fret}/{note.position.string}"
         elements.append(note_el)
 
         if note.elapsed_beats.division == 1:
             elements.append("|")
+
+    if elements[-1:] == '|':
+        elements[-1:] = ':|'
+    else:
+        elements.append(':|')
 
     return " ".join(elements)
