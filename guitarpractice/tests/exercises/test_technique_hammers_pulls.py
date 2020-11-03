1aaaad093de00f2b3ba7e07ace3101f3e618f82a
Craig Richardson <craigargh@gmail.com>
2020-11-03 22:28:13 +0000

    Add tests for level 2 hammers/pulls

--- guitarpractice/tests/exercises/test_technique_hammers_pulls.py
+++ guitarpractice/tests/exercises/test_technique_hammers_pulls.py
@@ -13,3 +13,19 @@ class TestHammersAndPulls(TestCase):
 
         self.assertEqual(8, len(result.notes))
         self.assertTrue(all(Beat(1, 8) == note.duration for note in result.notes))
+
+    def test_level_two_has_eighth_notes(self):
+        random.seed(10)
+
+        result = technique_hammers_pulls(variation='level-2')
+
+        self.assertEqual(8, len(result.notes))
+        self.assertTrue(all(Beat(1, 8) == note.duration for note in result.notes))
+
+    def test_level_two_can_have_eighteenth_notes(self):
+        random.seed(3)
+
+        result = technique_hammers_pulls(variation='level-2')
+
+        self.assertEqual(16, len(result.notes))
+        self.assertTrue(all(Beat(1, 16) == note.duration for note in result.notes))
