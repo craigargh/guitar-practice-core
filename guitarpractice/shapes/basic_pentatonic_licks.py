3d93d044cd90f14405087c46d5865464cb199ad0
craigargh <craigargh@gmail.com>
2020-11-10 19:45:40 +0000

    Add extra licks to pentatonic lick exercise

--- guitarpractice/shapes/basic_pentatonic_licks.py
+++ guitarpractice/shapes/basic_pentatonic_licks.py
@@ -4,6 +4,8 @@ from guitarpractice.models import Note, Beat, FretPosition
 def basic_pentatonic_licks():
     return [
         lick_one(),
+        lick_two(),
+        lick_three(),
     ]
 
 
@@ -16,4 +18,26 @@ def lick_one():
         Note(duration=Beat(1, 8), elapsed_beats=Beat(7, 8), position=FretPosition(8, 6), order=4),
         Note(duration=Beat(1, 8), elapsed_beats=Beat(8, 8), position=FretPosition(7, 4), order=5),
         Note(duration=Beat(1, 1, tie=True), elapsed_beats=Beat(2, 1), position=FretPosition(7, 4), order=6),
+    ]
+
+
+def lick_two():
+    return [
+        Note(duration=Beat(1, 4, rest=True), elapsed_beats=Beat(1, 4), position=None, order=0),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(3, 8), position=FretPosition(5, 4), order=1),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(4, 8), position=FretPosition(7, 5), order=2),
+        Note(duration=Beat(1, 4), elapsed_beats=Beat(3, 4), position=FretPosition(5, 4), order=3),
+        Note(duration=Beat(1, 4), elapsed_beats=Beat(4, 4), position=FretPosition(7, 4), order=4),
+    ]
+
+
+def lick_three():
+    return [
+        Note(duration=Beat(1, 8, rest=True), elapsed_beats=Beat(1, 8), position=None, order=0),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(2, 8), position=FretPosition(5, 1), order=1),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(3, 8), position=FretPosition(8, 2), order=2),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(4, 8), position=FretPosition(5, 2), order=3),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(5, 8), position=FretPosition(7, 3), order=4),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(6, 8), position=FretPosition(5, 3), order=5),
+        Note(duration=Beat(1, 4), elapsed_beats=Beat(7, 8), position=FretPosition(7, 4), order=6),
     ]
