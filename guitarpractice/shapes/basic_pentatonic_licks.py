27792555377ffb5be48fe13c33c7ec2259330e8c
craigargh <craigargh@gmail.com>
2020-11-10 19:38:48 +0000

    Correct lick order

--- guitarpractice/shapes/basic_pentatonic_licks.py
+++ guitarpractice/shapes/basic_pentatonic_licks.py
@@ -9,11 +9,11 @@ def basic_pentatonic_licks():
 
 def lick_one():
     return [
-        Note(duration=Beat(1, 4), elapsed_beats=Beat(1, 4), position=FretPosition(5, 6), order=1),
-        Note(duration=Beat(1, 4), elapsed_beats=Beat(1, 4), position=FretPosition(7, 4), order=1),
-        Note(duration=Beat(1, 8), elapsed_beats=Beat(1, 4), position=FretPosition(5, 4), order=1),
-        Note(duration=Beat(1, 8), elapsed_beats=Beat(1, 4), position=FretPosition(7, 5), order=1),
-        Note(duration=Beat(1, 8), elapsed_beats=Beat(1, 4), position=FretPosition(8, 6), order=1),
-        Note(duration=Beat(1, 8), elapsed_beats=Beat(1, 4), position=FretPosition(7, 4), order=1),
-        Note(duration=Beat(1, 1), elapsed_beats=Beat(1, 4, tie=True), position=FretPosition(7, 4), order=1),
+        Note(duration=Beat(1, 4), elapsed_beats=Beat(1, 4), position=FretPosition(5, 6), order=0),
+        Note(duration=Beat(1, 4), elapsed_beats=Beat(2, 4), position=FretPosition(7, 4), order=1),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(5, 8), position=FretPosition(5, 4), order=2),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(6, 8), position=FretPosition(7, 5), order=3),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(7, 8), position=FretPosition(8, 6), order=4),
+        Note(duration=Beat(1, 8), elapsed_beats=Beat(8, 8), position=FretPosition(7, 4), order=5),
+        Note(duration=Beat(1, 1, tie=True), elapsed_beats=Beat(2, 1), position=FretPosition(7, 4), order=6),
     ]
