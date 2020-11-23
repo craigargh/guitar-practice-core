1b821f0b9cd24eff16513960d185cd59e4cbdfeb
craigargh <craigargh@gmail.com>
2020-11-23 18:48:31 +0000

    Fix riffs exercise arguments

--- guitarpractice/exercises/metal_picked_riffs.py
+++ guitarpractice/exercises/metal_picked_riffs.py
@@ -1,6 +1,7 @@
 import random
 from functools import partial
 
+from guitarpractice.models import Beat
 from guitarpractice.pickpatterns import fixed_order_pattern
 from guitarpractice.sequencer import make_sequence
 from guitarpractice.shapes import major_scale_shapes
@@ -17,7 +18,10 @@ def metal_picked_riffs(variation=None):
     preset_pattern = random.choice(level_one_picked_metal_patterns())
     pick_pattern = partial(fixed_order_pattern, pattern=preset_pattern)
 
+    rhythm = [Beat(1, 8)]
+
     return make_sequence(
         [shape],
-        pick_pattern=pick_pattern
+        pick_pattern=pick_pattern,
+        rhythm=rhythm,
     )
