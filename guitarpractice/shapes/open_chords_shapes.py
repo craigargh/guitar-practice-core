a8b67e76997710899dccab2f4dbb5df081d1e9b7
craigargh <craigargh@gmail.com>
2021-08-04 18:07:24 +0100

    Add minor open chords

--- guitarpractice/shapes/open_chords_shapes.py
+++ guitarpractice/shapes/open_chords_shapes.py
@@ -53,6 +53,52 @@ def open_major_root_4_var_1():
         FretPosition(string=4, fret=0, highlighted=True),
         FretPosition(string=3, fret=2, finger=2),
         FretPosition(string=2, fret=3, finger=3, highlighted=True),
-        FretPosition(string=1, fret=1, finger=1)
+        FretPosition(string=1, fret=2, finger=1)
     ]
     return GuitarShape('Major', 'chord', positions, tonality='maj', frets_movable=False)
+
+
+def open_minor_root_6_var_1():
+    positions = [
+        FretPosition(string=6, fret=0, highlighted=True),
+        FretPosition(string=5, fret=2, finger=2),
+        FretPosition(string=4, fret=2, finger=3, highlighted=True),
+        FretPosition(string=3, fret=0),
+        FretPosition(string=2, fret=0),
+        FretPosition(string=1, fret=0, highlighted=True),
+    ]
+    return GuitarShape('Minor', 'chord', positions, tonality='min', frets_movable=False)
+
+
+def open_minor_root_6_var_2():
+    positions = [
+        FretPosition(string=6, fret=3, finger=2, highlighted=True),
+        FretPosition(string=5, fret=1, finger=1),
+        FretPosition(string=4, fret=0),
+        FretPosition(string=3, fret=0, highlighted=True),
+        FretPosition(string=2, fret=0),
+        FretPosition(string=1, fret=3, finger=3, highlighted=True),
+    ]
+    return GuitarShape('Minor', 'chord', positions, tonality='min', frets_movable=False)
+
+
+def open_minor_root_5_var_1():
+    positions = [
+        FretPosition(string=5, fret=0, highlighted=True),
+        FretPosition(string=4, fret=2, finger=3),
+        FretPosition(string=3, fret=2, finger=2, highlighted=True),
+        FretPosition(string=2, fret=1, finger=1),
+        FretPosition(string=1, fret=0, highlighted=True)
+    ]
+
+    return GuitarShape('Minor', 'chord', positions, tonality='min', frets_movable=False)
+
+
+def open_minor_root_4_var_1():
+    positions = [
+        FretPosition(string=4, fret=0, highlighted=True),
+        FretPosition(string=3, fret=2, finger=2),
+        FretPosition(string=2, fret=3, finger=3, highlighted=True),
+        FretPosition(string=1, fret=1, finger=1)
+    ]
+    return GuitarShape('Minor', 'chord', positions, tonality='min', frets_movable=False)
